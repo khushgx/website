d6993cae849e7f17e98d8fde2241dea881c49afb
khushgx <khushgx@gmail.com>
2023-11-16 22:45:01 -0600

    k

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,59 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-white">
+      <header className="py-6 mt-8 container mx-auto max-w-2xl">
+        <div className="flex flex-col md:flex-row items-start">
+          <div className="mb-4 md:mb-0 md:mr-4">
+            <h1 className="text-3xl font-bold text-gray-800">Khush Gupta</h1>
+            <p className="text-gray-700  mt-2">
+            is an <span className="font-bold">academic</span>, a <span className="font-bold">developer</span>, and a <span className="font-bold">builder</span>.
+            </p>
+          </div>
+          <nav className="flex space-x-4 ml-0 md:ml-auto">
+            <Link href="https://drive.google.com/file/d/14kFWr9PVHWh05w4zSt5UNnTILBM4dJiZ/view?usp=sharing" className="text-lg text-gray-700 hover:text-gray-900" target="_blank">Resume</Link>
+          </nav>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main className="container mx-auto p-6 max-w-2xl">
+        <section className="mb-16">
+        <p className="text-gray-700">
+  I'm working on <a href="https://discus.ai/" className="underline hover:text-blue-500" target="_blank" rel="noopener noreferrer">Discus.ai</a>, an open source project to enhance AI interactions, Synthetic Text Generation, and LLM Fine-Tuning. Currently studying in the Jerome Fisher M&T Program at the University of Pennsylvania, focusing on Computer Science and Statistics.
+</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <h2 className="text-2xl font-semibold text-gray-800 mt-12 mb-4">Notable Projects</h2>
+          <div className="grid grid-cols-1 gap-4">
+            {/* Repeat this block for each project */}
+            <div className="p-4 border rounded hover:shadow-lg transition duration-300">
+              <Link href="https://github.com/discus-labs/discus"><span className="font-semibold text-lg text-gray-800 hover:text-gray-600 cursor-pointer">Discus.ai</span></Link>
+            </div>
+            {/* End of project block */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+             {/* Repeat this block for each project */}
+             <div className="p-4 border rounded hover:shadow-lg transition duration-300">
+              <Link href="https://github.com/hvnguyen57/hackharvard"><span className="font-semibold text-lg text-gray-800 hover:text-gray-600 cursor-pointer">Custom Programming Language and Compiler</span></Link>
+            </div>
+            {/* End of project block */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+             {/* Repeat this block for each project */}
+             <div className="p-4 border rounded hover:shadow-lg transition duration-300">
+              <Link href="https://github.com/khushgx/cutom_programming_compiler"><span className="font-semibold text-lg text-gray-800 hover:text-gray-600 cursor-pointer">Solar Up With Us</span></Link>
+            </div>
+            {/* End of project block */}
+          </div>
+        </section>
+      </main>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="mt-4 py-8 container mx-auto max-w-2xl">
+        <div className="border-b-2 border-gray-200 mb-4"></div>
+        <p className="text-gray-700">
+          LinkedIn: <span className="border-b-2 border-dotted border-gray-400 ml-2 mr-2"></span><Link href="https://linkedin.com/in/khushg"><span className="text-blue-500 hover:underline">@khushg</span></Link><br />
+          GitHub: <span className="border-b-2 border-dotted border-gray-400 ml-2 mr-2"></span><Link href="https://github.com/khushgx"><span className="text-blue-500 hover:underline">@khushgx</span></Link><br />
+          Email: <span className="border-b-2 border-dotted border-gray-400 ml-2 mr-2"></span><Link href="mailto:khushgx@gmail.com"><span className="text-blue-500 hover:underline">khushgx@gmail.com</span></Link>
+        </p>
+      </footer>
+    </div>
+  );
 }
